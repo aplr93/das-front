fb8b5f1ce5b57f72a85a230efed4a09a8fdbd5b2
Bruno <brunocmpr@gmail.com>
2021-09-23 22:02:09 -0300

    Creates Client Module

--- das-front/src/app/app.module.ts
+++ das-front/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ClienteModule } from './cliente';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClienteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
